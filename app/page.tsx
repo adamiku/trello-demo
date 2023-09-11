c6041343f7f410fdff8bea54fc5302828e64089e
Adam <adam_mikulas@mckinsey.com>
2023-09-11 17:19:19 +0200

    add appwrite setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      {/* Board */}
-      <h1>Trello 2.0 AI Clone</h1>
+      <Board />
     </main>
   );
 }
