aacd89781c22e040147fa92d1743ee8829ab0a6b
Adam <adam_mikulas@mckinsey.com>
2023-09-11 16:57:42 +0200

    add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main>
+      <Header />
+      {/* Board */}
       <h1>Trello 2.0 AI Clone</h1>
     </main>
   );
