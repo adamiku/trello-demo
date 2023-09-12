719b38188874ab4353bab0e0121055257428d762
Adam <adam_mikulas@mckinsey.com>
2023-09-12 21:42:55 +0200

    add delete functionality

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useBoardStore } from "@/store/BoardStore";
 import { XCircleIcon } from "@heroicons/react/24/solid";
 import {
   DraggableProvidedDragHandleProps,
@@ -23,6 +24,7 @@ function TodoCard({
   dragHandleProps,
   draggableProps,
 }: Props) {
+  const deleteTask = useBoardStore((state) => state.deleteTask);
   return (
     <div
       {...dragHandleProps}
@@ -32,7 +34,10 @@ function TodoCard({
     >
       <div className="flex justify-between items-center p-5">
         <p>{todo.title}</p>
-        <button className="text-red-500 hover:text-red-600">
+        <button
+          onClick={() => deleteTask(index, todo, id)}
+          className="text-red-500 hover:text-red-600"
+        >
           <XCircleIcon className="ml-5 h-8 w-8" />
         </button>
       </div>
